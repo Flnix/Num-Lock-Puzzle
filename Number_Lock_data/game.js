6df2f1574fedbf56b498f7d4ef2846afc9a1ea35
Deepan V <vdeepan272@proton.me>
2024-08-04 11:08:05 +0530

    Update game.js

--- Number_Lock_data/game.js
+++ Number_Lock_data/game.js
@@ -201,7 +201,7 @@ function check_the_screen_code_result() {
   if(player_win){
     G.SetGameEnd(true); //End the game and set (player has win the game).
     G.PlaySound(sound.correct); // play sound correct
-    alert("Flag: Logic_Puli}")
+    alert("Flag: Logic_Tiger}")
   }
   else {
     var clone_array = [...screen_code]; // clone the current screen_code array
